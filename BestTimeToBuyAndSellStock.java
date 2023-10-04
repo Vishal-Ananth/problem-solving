68f1ce8b2141f6f17bb5dda8047cb310cdc9c699
Vishal Ananth J <imvishal2002@gmail.com>
2023-10-04 23:43:08 +0530

    Leetcode: 121.Best Time to Buy and Sell Stock

---
+++ BestTimeToBuyAndSellStock.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class BestTimeToBuyAndSellStock {
+    public static void main(String[] args) {
+        int[] prices = {1,2};
+        System.out.println(maxProfit(prices));
+    }
+
+    public static int maxProfit(int[] prices) {
+        int overall=0;
+
+        int left=Integer.MAX_VALUE;
+
+        for(int i:prices){
+            if(i<left){
+                left=i;
+            }
+            if(overall<left-i){
+                overall=left-i;
+            }
+
+        }
+        return overall;
+    }
+}
