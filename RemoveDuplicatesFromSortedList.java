795b4b15043204a1cd0dac0ea6cb1074a6fbd131
Vishal Ananth J <imvishal2002@gmail.com>
2023-10-05 00:11:04 +0530

    Leetcode: 83.Remove Duplicates from Sorted List - declared class ListNode

--- RemoveDuplicatesFromSortedList.java
+++ RemoveDuplicatesFromSortedList.java
@@ -1,14 +1,12 @@
 public class RemoveDuplicatesFromSortedList {
-    /**
-     * Definition for singly-linked list.
-     * public class ListNode {
-     *     int val;
-     *     ListNode next;
-     *     ListNode() {}
-     *     ListNode(int val) { this.val = val; }
-     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-     * }
-     */
+    // Definition for singly-linked list.
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
     class Solution {
         public ListNode deleteDuplicates(ListNode head) {
             ListNode temp=head;
