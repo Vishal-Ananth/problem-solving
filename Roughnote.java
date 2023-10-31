6384fc5bb58f7c3f9b0d1c70f61ad311d4ceb539
Vishal Ananth J <imvishal2002@gmail.com>
2023-10-31 10:21:15 +0530

    Leetcode : 1207. Unique Number of Occurrences

--- Roughnote.java
+++ Roughnote.java
@@ -2,9 +2,23 @@ import java.util.*;
 
 public class Roughnote {
     public static void main(String[] args) {
-        String s="A man, a plan, a canal: Panama";
-        s=s.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
-        System.out.println(s);
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int[] arr ={1,2,2,1,1,3,2};
+        for(int i:arr){
+            map.put(i,map.getOrDefault(i,1)+1);
+        }
+        System.out.println(map);
+
+        HashSet<Integer> set = new HashSet<>();
+        for(Integer i: map.values()){
+            if(set.contains(i)){
+                System.out.println("False");
+                return;
+            }
+            set.add(i);
+        }
+        System.out.println(set);
+        System.out.println("True");
     }
 
 }
